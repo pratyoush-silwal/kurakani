fcdb520d78796dddfcab473348c24fb49ce9cf27
sasudo2 <71052914+sasudo2@users.noreply.github.com>
2024-11-19 19:52:26 +0545

    Update chat_message.hpp

--- chat_message.hpp
+++ chat_message.hpp
@@ -79,22 +79,22 @@ public:
 
         int body_len;
         char m_id[5] = ""; // Ensure m_id is zero-terminated
-        double s_id;
-        double r_id;
+        std::uint64_t s_id;
+        std::uint64_t r_id;
 
-        std::cout << "decoding header" << std::endl;
-
+		std::cout << "decoding header" << std::endl;
+        std::cout << "Recieved_header: " << header << std::endl;
         if (std::sscanf(header, "%4d%4s%20llu%20llu", &body_len, m_id, &s_id, &r_id) != 4)
         {
             body_length_ = 0;
             return false;
         }
-        std::cout << "decode_header successfull" << std::endl;
+		std::cout << "decode_header successfull" << std::endl;
         body_length_ = body_len;
         message_id = m_id;
         sender_id = s_id;
         receiver_id = r_id;
-        std::cout << "sender_id: " << sender_id << std::endl;
+		std::cout << "sender_id: " << sender_id << std::endl;
         if (body_length_ > max_body_length)
         {
             body_length_ = 0;
@@ -110,6 +110,7 @@ public:
         receiver_id = r_id;
         char header[header_length + 1] = "";
         std::sprintf(header, "%4d%4s%20llu%20llu", static_cast<int>(body_length_), message_id.c_str(), sender_id, receiver_id); // the larges size these arguments can occupy is much larger so stack over flow may occure
+		std::cout << "Encoded header: " << header << "'\n";
         std::memcpy(data_, header, header_length);
     }
 
@@ -133,94 +134,3 @@ private:
 };
 
 #endif // CHAT_MESSAGE_HPP
-
-// //
-// // chat_message.hpp
-// // ~~~~~~~~~~~~~~~~
-
-
-// #ifndef CHAT_MESSAGE_HPP
-// #define CHAT_MESSAGE_HPP
-// #pragma warning(disable : 4996)
-
-// #include <cstdio>
-// #include <cstdlib>
-// #include <cstring>
-
-// class chat_message
-
-// {
-// public:
-//     enum { header_length = 4 };
-//     enum { max_body_length = 512 };
-
-//     chat_message()
-//         : body_length_(0), sender_id(0), receiver_id(0)
-//     {
-//     }
-
-//     const char* data() const
-//     {
-//         return data_;
-//     }
-
-//     char* data()
-//     {
-//         return data_;
-//     }
-
-//     std::size_t length() const
-//     {
-//         return header_length + body_length_;
-//     }
-
-//     const char* body() const
-//     {
-//         return data_ + header_length;
-//     }
-
-//     char* body()
-//     {
-//         return data_ + header_length;
-//     }
-
-//     std::size_t body_length() const
-//     {
-//         return body_length_;
-//     }
-
-//     void body_length(std::size_t new_length)
-//     {
-//         body_length_ = new_length;
-//         if (body_length_ > max_body_length)
-//             body_length_ = max_body_length;
-//     }
-
-//     bool decode_header()
-//     {
-//         char header[header_length + 1] = "";
-//         std::strncat(header, data_, header_length);
-//         body_length_ = std::atoi(header);
-//         if (body_length_ > max_body_length)
-//         {
-//             body_length_ = 0;
-//             return false;
-//         }
-//         return true;
-//     }
-
-//     void encode_header()
-//     {
-//         char header[header_length + 1] = "";
-//         std::sprintf(header, "%4d", static_cast<int>(body_length_));
-//         std::memcpy(data_, header, header_length);
-//     }
-
-// private:
-//     double sender_id;
-//     double receiver_id;
-//     char data_[header_length + max_body_length];
-//     std::size_t body_length_;
-// };
-
-// #endif // CHAT_MESSAGE_HPP
